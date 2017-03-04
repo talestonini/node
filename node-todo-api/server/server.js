308cd536886e0cd8ba31e4aaab86165be206f7d7
Tales Tonini <talestonini@gmail.com>
2017-03-04 19:26:44 +1100

    more from NodeJS course

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,6 +1,7 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -10,23 +11,16 @@ let app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  var newTodo = new Todo({
-    text: req.body.text
-  });
-
-  newTodo.save().then((todo) => {
-    res.status(201).send(todo);
-  }).catch((e) => {
-    res.status(400).send(e);
-  });
+  new Todo({ text: req.body.text })
+    .save()
+    .then((todo) => res.status(201).send(todo))
+    .catch((e) => res.status(400).send(e));
 });
 
 app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send({ todos });
-  }).catch((e) => {
-    res.status(400).send(e);
-  });
+  Todo.find()
+    .then((todos) => res.send({ todos }))
+    .catch((e) => res.status(400).send(e));
 });
 
 app.get('/todos/:id', (req, res) => {
@@ -35,14 +29,14 @@ app.get('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findById(id).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
-    res.send({ todo });
-  }).catch((e) => {
-    res.status(400).send(e);
-  });
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch((e) => res.status(400).send(e));
 });
 
 app.delete('/todos/:id', (req, res) => {
@@ -51,14 +45,38 @@ app.delete('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findByIdAndRemove(id).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
-    res.send({ todo });
-  }).catch((e) => {
-    res.status(400).send(e);
-  });
+  Todo.findByIdAndRemove(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch((e) => res.status(400).send(e));
+});
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch((e) => res.status(400).send(e));
 });
 
 const SERVER_PORT = process.env.PORT || 3000;
