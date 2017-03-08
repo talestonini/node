e5877c8b5c2da8af93269fa7edb617ef88825488
Tales Tonini <talestonini@gmail.com>
2017-03-08 16:11:38 +1100

    Odecee NodeJS API training: initial (cont...)

--- node-js-api-training/server/service/tests/zoopla.test.js
+++ node-js-api-training/server/service/tests/zoopla.test.js
@@ -15,19 +15,29 @@ let loadResponse = file => JSON.parse(fs.readFileSync(`${__dirname}/stubs/${file
 const responses = [
   [200, loadResponse('200_zoopla_default')],
   [200, loadResponse('200_zoopla_disambiguation')],
-  [400, loadResponse('400_zoopla_unknown_location')]
+  [400, loadResponse('400_zoopla_unknown_location')],
+  [200, loadResponse('200_zoopla_price_range_1-9')],
+  [200, loadResponse('200_zoopla_price_range_2-9')],
+  [200, loadResponse('200_zoopla_price_range_3-9')],
+  [200, loadResponse('200_zoopla_price_range_4-9')],
+  [200, loadResponse('200_zoopla_price_range_5-9')],
+  [200, loadResponse('200_zoopla_price_range_6-9')],
+  [200, loadResponse('200_zoopla_price_range_7-9')],
+  [200, loadResponse('200_zoopla_price_range_8-9')],
+  [200, loadResponse('200_zoopla_price_range_9-9')]
 ];
 
 describe('Zoopla service', () => {
   before(() => mockAxiosAdapter.onAny().reply(config => responses.shift()));
 
-  it('should import properties using default parameters', () => {
-    return zoopla.importProperties({})
+  it('should import single page properties using default parameters', () => {
+    return zoopla.importPageProperties({})
       .then(response => {
-        expect(response.count).toBe(100);
+        expect(response).toInclude({ count: 404075, importCount: 100 });
         expect(response.properties).toExist('response has no properties');
         expect(response.properties.length).toBe(100);
         response.properties.forEach(property => {
+          expect(property._id).toExist();
           expect(property.__v).toBeA('number');
           expect(property.listingId).toExist('response property has no listingId');
           Property.findOne({ listingId: property.listingId })
@@ -37,7 +47,7 @@ describe('Zoopla service', () => {
   });
 
   it('should handle disambiguation error response', () => {
-    return zoopla.importProperties({ area: 'Melbourne' })
+    return zoopla.importPageProperties({ area: 'Melbourne' })
       .then(response => {
         expect(response).toInclude({
           httpStatus: 200,
@@ -48,13 +58,24 @@ describe('Zoopla service', () => {
   });
 
   it('should handle unknown location error response', () => {
-    return zoopla.importProperties({ country: 'Ireland' })
+    return zoopla.importPageProperties({ country: 'Ireland' })
       .then(response => {
         expect(response).toInclude({
           httpStatus: 400,
           errorCode: '7',
           errorMessage: 'Unknown location entered.'
         });
+      });
+  });
+
+  it('should import multiple page properties', () => {
+    let count = 854;
+    return zoopla.importProperties({ minimumPrice: 998000, maximumPrice: 1000000 })
+      .then(response => {
+        expect(response.count).toBe(count, `count should be ${count}, but is ${response.count}`);
+        expect(response.importCount).toBe(count, `importCount should be ${count}, but is ${response.importCount}`);
+        expect(response.properties.length)
+          .toBe(count, `properties length should be ${count}, but is ${response.properties.length}`);
       });
   });
 });
--- node-js-api-training/server/service/zoopla.js
+++ node-js-api-training/server/service/zoopla.js
@@ -1,66 +1,90 @@
 let axios = require('axios');
 const decamelize = require('decamelize');
+const merge = require('merge');
+const _ = require('lodash');
 
 const {Property} = require('../model/property');
 
 const API_KEY = 'xexa5zsx55mttszpd9yuykj3';
-const IMPORT_PROP_DEFAULT_FILTERS = {
-  country: 'England',
-  outcode: 'BR1',
-  pageNumber: 1,
-  pageSize: 100,
-};
+const DEFAULT_FILTERS = { country: 'England', outcode: 'BR1', pageNumber: 1, pageSize: 100 };
 
-let importProperties = filters => {
-  let params = filters2Params(filters);
-  let zooplaUrl = `http://api.zoopla.co.uk/api/v1/property_listings.json?api_key=${API_KEY}${params}`;
-  console.log(`GET ${zooplaUrl}`);
+/**
+ * Imports properties from Zoopla's listings into the database.
+ * 
+ * If a page number is provided within the filters, properties will be imported from that page onwards, otherwise from
+ * page IMPORT_PROP_DEFAULT_FILTERS.pageNumber. All the pages will be processed until the end or pages limit is reached.
+ */
+let importProperties = (filters, pagesLimit = Number.MAX_SAFE_INTEGER) => {
+  filters = merge(DEFAULT_FILTERS, filters);
+  return importPageProperties(filters)
+    .then(firstImport => {
+      let totalPages = Math.ceil(firstImport.count / filters.pageSize);
+      let lastPage = Math.min(totalPages, filters.pageNumber + pagesLimit - 1);
+      console.log(`--------> pages ${filters.pageNumber} to ${lastPage} (total ${totalPages})`);
+      console.log(`--------> count`, firstImport.count);
+
+      let pageImportPromises = [];
+      for (let i = filters.pageNumber + 1; i <= lastPage; i++) { // first page already imported
+        filters.pageNumber = i;
+        pageImportPromises.push(importPageProperties(filters));
+      }
+
+      let aggregation = firstImport;
+      return Promise.all(pageImportPromises)
+        .then(imports => {
+          imports.forEach(_import => {
+            aggregation.importCount += _import.importCount;
+            Array.prototype.push.apply(aggregation.properties, _import.properties);
+          });
+          console.log('-------->', {
+            count: aggregation.count,
+            importCount: aggregation.importCount,
+            'properties.length': aggregation.properties.length
+          });
+          return aggregation;
+        });
+    });
+}
+
+/**
+ * Imports 1 page worth of properties from Zoopla's listings into the database.
+ */
+let importPageProperties = filters => {
+  let zooplaUrl = `http://api.zoopla.co.uk/api/v1/property_listings.json?api_key=${API_KEY}${uriParams(filters)}`;
+  console.log(`page ${filters.pageNumber} -> GET ${zooplaUrl}`);
   return axios.get(zooplaUrl)
     .then(response => {
       if (response.status === 200 && response.data.error_code) {
         return errorResponse(response);
       }
 
+      let count = response.data.result_count;
       let persistPromises = [];
       response.data.listing.forEach(listing => {
-        let property = listing2Property(listing, response.data.postcode, response.data.country);
+        let property = mapToProperty(listing, response.data.postcode, response.data.country);
         persistPromises.push(persistProperty(property));
       });
-      return Promise.all(persistPromises).then((properties) => {
+      return Promise.all(persistPromises).then(properties => {
         return {
-          count: properties.length,
-          properties
+          count,
+          importCount: properties.length,
+          properties: _.map(properties, p => _.pick(p, ['_id', '__v', 'listingId']))
         };
       });
     }, error => errorResponse(error.response));
 };
 
-let filters2Params = filters => {
+let uriParams = filters => {
   let params = '';
-  for (let defaultFilter in IMPORT_PROP_DEFAULT_FILTERS) {
-    if (!filters[defaultFilter]) {
-      params += reqParam(defaultFilter, IMPORT_PROP_DEFAULT_FILTERS[defaultFilter]);
-    }
-  }
   for (let filter in filters) {
-    params += reqParam(filter, filters[filter]);
+    params += param(filter, filters[filter]);
   }
   return params;
 };
 
-let reqParam = (name, value) => `&${encodeURIComponent(decamelize(name))}=${encodeURIComponent(value)}`;
+let param = (name, value) => `&${encodeURIComponent(decamelize(name))}=${encodeURIComponent(value)}`;
 
-let errorResponse = response => {
-  console.log('error querying Zoopla:', response.status);
-  console.log(response.data);
-  return {
-    httpStatus: response.status,
-    errorCode: response.data.error_code,
-    errorMessage: response.data.error_string
-  };
-};
-
-let listing2Property = (listing, postcode, country) => {
+let mapToProperty = (listing, postcode, country) => {
   return {
     listingId: listing.listing_id,
     url: listing.details_url,
@@ -86,11 +110,22 @@ let listing2Property = (listing, postcode, country) => {
 };
 
 let persistProperty = property => {
-  return Property.findOneAndRemove({ listingId: property.listingId })
-    .then(() => new Property(property).save())
+  return Property.findOneAndUpdate({ listingId: property.listingId }, { $set: property }, { new: true })
+    .then((updatedProperty) => updatedProperty || new Property(property).save())
     .catch(e => console.log('error persisting property:', e));
 }
 
+let errorResponse = response => {
+  console.log('error querying Zoopla:', response.status);
+  console.log(response.data);
+  return {
+    httpStatus: response.status,
+    errorCode: response.data.error_code,
+    errorMessage: response.data.error_string
+  };
+};
+
 module.exports = {
-  importProperties
+  importProperties,
+  importPageProperties
 }
